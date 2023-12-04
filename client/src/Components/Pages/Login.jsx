b889ac6410e65a6563ad3d3961e69e83aef4eff9
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2023-12-04 09:33:23 +0200

    Created the signup and login files

--- client/src/Components/Pages/Login.jsx
+++ client/src/Components/Pages/Login.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import {React, useState} from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/login", { email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <div className="login">
+      <h4>
+        Welcome Back !
+        <span id="wave" role="img" aria-labelledby="wave">
+          👋🏻
+        </span>{" "}
+      </h4>
+      <div style={{ fontSize: "200px"}}>
+        <UserOutlined />
+      </div>
+
+      <Form
+        name="normal_login"
+        className="login-form"
+        onFinish={onFinish}
+        onSubmit={handleSubmit}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            Forgot Password ?
+          </Link>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
+export default Login;
--- client/src/Components/Pages/Signup.jsx
+++ client/src/Components/Pages/Signup.jsx
@@ -1,11 +1,175 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Input, Checkbox, Button } from "antd";
+import { auth, provider } from "./config";
+import { signInWithPopup } from "firebase/auth";
+import axios from "axios";
 
 function Signup() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/signup", { email, password })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
+  // --------------------------SignUp with Google (Firebase)--------------------------------------------------
+
+  const [value, setValue] = useState("");
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setValue(data.user.email);
+      localStorage.setItem("email", data.user.email);
+    });
+  };
+
+  useEffect(() => {
+    setValue(localStorage.getItem("email"));
+  }, []);
+
+  // ---------------------------Form Fields---------------------------------------------------------
+
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  const formItemLayout = {
+    labelCol: {
+      span: 24,
+    },
+    wrapperCol: {
+      span: 24,
+    },
+    vertical: true,
+  };
+
   return (
-    <div>
-      SignUp
+    <div className="signup">
+      <h4>Sign Up for TaskMaster</h4>
+      <p
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: "10px",
+        }}
+      >
+        Create an account or
+        <div style={{ marginLeft: "8px" }}>
+          <Link to="/login" style={{ textDecoration: "none" }}>
+            login
+          </Link>
+        </div>
+      </p>
+      <Form
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        onSubmit={handleSubmit}
+        initialValues={{
+          prefix: "86",
+        }}
+        style={{ maxWidth: 400 }}
+      >
+        <Form.Item
+          {...formItemLayout}
+          name="email"
+          label="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          rules={[
+            {
+              type: "email",
+              message: "Please enter a valid email address.",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          {...formItemLayout}
+          name="password"
+          label="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          rules={[
+            {
+              required: true,
+              message: "Please input your password.",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          {...formItemLayout}
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password.",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject("The passwords do not match.");
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          {...formItemLayout}
+          name="agreement"
+          valuePropName="checked"
+          rules={[
+            {
+              validator: (_, value) =>
+                value
+                  ? Promise.resolve()
+                  : Promise.reject("You must accept the agreement."),
+            },
+          ]}
+        >
+          <Checkbox>
+            I agree to TaskMaster's user agreement and privacy policy.
+          </Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Sign Up
+          </Button>
+        </Form.Item>
+      </Form>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <hr style={{ flex: 1, borderTop: "2px solid #333" }} />
+        <p style={{ margin: "0 10px", color: "#333" }}>OR</p>
+        <hr style={{ flex: 1, borderTop: "2px solid #333" }} />
+      </div>
+
+      <div className="auth-btn">
+        <Button onClick={handleClick}>
+          <i className="fab fa-google"></i> Continue with Google
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
