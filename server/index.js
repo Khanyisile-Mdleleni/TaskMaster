747bcc58c602e86473823fbb78c6c285348818b3
Khanyisile Mdleleni <khanyisilemdleleni@gmail.com>
2023-12-05 11:50:00 +0200

    DB connection added

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require('cors') 
-const { mongoose } = require('mongoose')
+const mongoose  = require('mongoose')
 
 // DB Connection
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('database connected'))
-.catch(() => console.log('database not connected'))
+.catch((err) => console.log('database not connected', err))
 
 // Express Setup
 const app = express();
